517f6b645d494bf179404ea7d1f572635e25d346
Abbi H Safaie <Abbi@Abbis-MacBook-Air.local>
2014-04-16 21:20:34 +1000

    storyBoard changed and file place

---
+++ Favourites/favListDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  favListDetailViewController.h
+//  Favourites
+//
+//  Created by Abbi H Safaie on 16/04/2014.
+//  Copyright (c) 2014 Abbas H Safaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface favListDetailViewController : UIViewController
+
+@end
---
+++ Favourites/favListMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  favListMasterViewController.h
+//  Favourites
+//
+//  Created by Abbi H Safaie on 16/04/2014.
+//  Copyright (c) 2014 Abbas H Safaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface favListMasterViewController : UITableViewController
+
+@end
